6ced27513796d2fcd60248cb6368d5ce8e4e7151
Vincent <b07209016@ntu.edu.tw>
2023-08-02 18:14:59 +0800

    Add main and download scripts

---
+++ 0_download.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+wget https://raw.githubusercontent.com/ChungChenWei/single-node-k8s-install/main/1_basic_setup.sh
+wget https://raw.githubusercontent.com/ChungChenWei/single-node-k8s-install/main/2_install_docker.sh
+wget https://raw.githubusercontent.com/ChungChenWei/single-node-k8s-install/main/3_install_k8s.sh
+wget https://raw.githubusercontent.com/ChungChenWei/single-node-k8s-install/main/4_start_control_node.sh
+wget https://raw.githubusercontent.com/ChungChenWei/single-node-k8s-install/main/5_post_install.sh
+wget https://raw.githubusercontent.com/ChungChenWei/single-node-k8s-install/main/6_install_CNI.sh
+wget https://raw.githubusercontent.com/ChungChenWei/single-node-k8s-install/main/7_install_ingress_nginx.sh
+wget https://raw.githubusercontent.com/ChungChenWei/single-node-k8s-install/main/A_install_tools.sh
+
+chmod 755 *.sh
